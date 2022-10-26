50259a671cb1875d6197c829a4bf07b71db1ed56
joacir <joacirprog@gmail.com>
2022-10-26 13:43:16 -0300

    form completed

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -18,6 +18,8 @@ map.on('click', event => {
   const lat = event.latlng.lat;
   const lng = event.latlng.lng;
   
+  document.querySelector('[name="lat"]').value = lat;
+  document.querySelector('[name="lng"]').value = lng;
   // remove icon 
   marker && map.removeLayer(marker);
 
@@ -26,3 +28,59 @@ map.on('click', event => {
     .addTo(map);
 });
 
+// add photos
+function addPhotoField() {
+  // Pegar o container de fotos #images
+  const container = document.querySelector('#images');
+
+  // pegar o container para duplicar .new-image
+  const fieldsContainer = document.querySelectorAll('.new-upload');
+
+  // Realizar o clone da última imagem adicionada
+  const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true);
+  
+  // Verificar se o campo está vazio, se sim, não adicionar ao container de imagens
+  const input = newFieldContainer.children[0]
+
+  if(input.value == '') {
+    return
+  }
+
+  // Limpar o campo antes de adicionar ao container de imagens
+  input.value = '';
+
+  // Adicionar o clone ao container de #images
+  container.appendChild(newFieldContainer);
+}
+
+function deleteField(event) {
+  const span = event.currentTarget;
+
+  const fieldsContainer = document.querySelectorAll('.new-upload');
+
+  if(fieldsContainer.length <= 1) {
+    // limpar o valor do campo   
+    span.parentNode.children[0].value = '';
+    return
+  }
+
+  // deletar o campo
+  span.parentNode.remove()
+}
+
+// select yes or no
+function toggleSelect(event) {
+  // retirar a class .active dos botões
+  document.querySelectorAll('.button-select button')
+    .forEach( button => button.classList.remove('active'));
+  // colocar a class .active no botão clicado
+    // pegar o botão clicado
+  const button = event.currentTarget;
+  button.classList.add('active')
+
+  // atualizar o meu input hidden com o valor selecionado
+  const input = document.querySelector('[name="open_weekends"]');
+  
+  // verificar se sim ou não
+  input.value = button.dataset.value
+}
