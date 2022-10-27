9f48e62ede9b7a3c067d74a1666d141c081910fc
joacir <joacirprog@gmail.com>
2022-10-27 10:54:21 -0300

    creat server and hbs

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,30 +1,39 @@
 // Create map
-const map = L.map('mapid').setView([-27.216263,-49.6430475], 16);
+const map = L.map("mapid").setView([-27.216263, -49.6430475], 16);
 
 // Create and add titleLayer
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // Create icon
 const icon = L.icon({
-  iconUrl: './public/images/map-marker.svg',
+  iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [170, 2]
+  popupAnchor: [170, 2],
 });
 
-// Create popup overlay
-const popup = L.popup({
-  closeButton: false,
-  className: 'map-popup',
-  minWidth: 240,
-  minHeight: 240
-}).setContent(`
-  Lar das meninas 
-  <a href="orphanage.html?id=1" class="choose-orphanage">
-    <img src="./public/images/arrow-white.svg">
-  </a>`);
+function addMarker({id, name, lat, lng}) {
+  // Create popup overlay
+  const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240,
+  }).setContent(`${name} <a href="/orphanage?id=${id}" class="choose-orphanage"><img src="/images/arrow-white.svg"></a>`);
 
-// Create and add marker
-L.marker([-27.216263,-49.6430475], { icon })
-  .addTo(map)
-  .bindPopup(popup)
+  // Create and add marker
+  L.marker([lat, lng], { icon }).addTo(map).bindPopup(popup);
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span');
+
+orphanagesSpan.forEach( span => {
+  const orphanage = {
+    id: span.dataset.id, 
+    name: span.dataset.name, 
+    lat: span.dataset.lat,
+    lng: span.dataset.lng
+  }
+
+  addMarker(orphanage);
+})
---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    id: 1,
+    lat: "-27.216263",
+    lng: "-49.6430475",
+    name: "Lar das menina",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1570570626315-95c19358f053?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aWR8fHx8fHwxNjY2ODc2MzEy&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
+
+      "https://images.unsplash.com/photo-1570570626315-95c19358f053?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aWR8fHx8fHwxNjY2ODc2MzEy&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: "1"
+  },
+
+  {
+    id: 2,
+    lat: "-27.216263",
+    lng: "-49.6530475",
+    name: "Lar dos meninos",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1570570626315-95c19358f053?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aWR8fHx8fHwxNjY2ODc2MzEy&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080",
+
+      "https://images.unsplash.com/photo-1570570626315-95c19358f053?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8aWR8fHx8fHwxNjY2ODc2MzEy&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas Das 18h até 8h",
+    open_on_weekends: "0"
+  }
+];
